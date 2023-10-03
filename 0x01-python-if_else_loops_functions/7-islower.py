7ee08408ba51a93b9e5d370c750a8c0feb84c942
Obada-Badee <obadabadee3@hotmail.com>
2023-10-03 18:33:50 +0000

    7. islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
-            print("{}".format(chr(ord(char) - 32)), end="")
-        else:
-            print("{}".format(char), end="")
+def islower(c):
+    if ord(c) >= 97 or <= 122:
+        return True
+    else:
+        return False
