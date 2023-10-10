487ccc91cf322fe8520dc8b8383ba2cb6e10d2a2
Obada-Badee <obadabadee3@hotmail.com>
2023-10-10 08:56:06 +0000

    9. fins the max Sol

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is not None:
-        maxn = my_list[0]
-        for i in my_list:
-            if i > maxn:
-                maxn = i
-        return maxn
-    return None
+    if my_list is None or my_list == []:
+        return None
+    maxn = my_list[0]
+    for i in my_list:
+        if i > maxn:
+            maxn = i
+    return maxn
