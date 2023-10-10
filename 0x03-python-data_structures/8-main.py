8d38a6a56ee4f92de2729a271cd9509eba07bdc8
Obada-Badee <obadabadee3@hotmail.com>
2023-10-10 08:47:38 +0000

    modify main file and fix sol

--- 0x03-python-data_structures/8-main.py
+++ 0x03-python-data_structures/8-main.py
@@ -3,4 +3,6 @@ multiple_returns = __import__('8-multiple_returns').multiple_returns
 
 sentence = "At school, I learnt C!"
 length, first = multiple_returns(sentence)
+length1, first1 = multiple_returns("")
 print("Length: {:d} - First character: {}".format(length, first))
+print("Length: {:d} - First character: {}".format(length1, first1))
