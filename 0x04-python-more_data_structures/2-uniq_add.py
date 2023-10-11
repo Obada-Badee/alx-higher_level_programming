82601eff8dee02ea3b4e84e6698adf201467e8ac
Obada-Badee <obadabadee3@hotmail.com>
2023-10-11 17:58:23 +0000

    2. Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     if my_list is None or my_list == []:
-        return None
+        return 0
     my_list.sort()
     sums = my_list[0]
     for i in range(1,len(my_list)):
-        if my_list[i] != my_list[i-1]:
+        if my_list[i] != my_list[i - 1]:
             sums += my_list[i]
     return sums
