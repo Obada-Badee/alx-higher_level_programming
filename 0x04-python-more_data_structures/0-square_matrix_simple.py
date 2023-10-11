ea242ada5f765df8d4b595097039e11d47ab2c96
Obada-Badee <obadabadee3@hotmail.com>
2023-10-11 17:20:13 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print()
+        return None
     else:
         sqr_matrix = []
         for row in matrix:
-            sq.append(list(map(lambda x: x**2, row)))
-        print(sq)
+            sqr_matrix.append(list(map(lambda x: x**2, row)))
+        return sqr_matrix
