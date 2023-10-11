ee1b937ef9ced7631953d782245fbe09d02eae5d
Obada-Badee <obadabadee3@hotmail.com>
2023-10-11 23:15:43 +0000

    15. Delete by value SOL

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys_tbd = []
+    for k, v in a_dictionary.items():
+        if v == value:
+            keys_tbd.append(k)
+
+    for key in keys_tbd:
+       del a_dictionary[key]
+    return a_dictionary
