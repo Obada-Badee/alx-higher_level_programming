fcf4ab304f86fe79c3e8c4d0e8d72c0efd9a27cc
Obada-Badee <obadabadee3@hotmail.com>
2023-10-25 23:38:26 +0000

    2. Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Empty class named aquares"""
+
+
+class Square:
+    """ Empty class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """ Initializes the square class"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
