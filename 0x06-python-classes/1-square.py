7167aea7f7b04af0305c7c036ce092dc451b3ac5
Obada-Badee <obadabadee3@hotmail.com>
2023-10-25 23:13:46 +0000

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Empty class named aquares"""
+
+
+class Square:
+    """ Empty class Square that defines a square"""
+    
+    def __init__(self, size):
+        """Initializes the data."""
+
+        self.__size = size
