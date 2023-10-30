aa189b00f3362c31030f93c1aa4dcf85b0cf41e5
Obada-Badee <obadabadee3@hotmail.com>
2023-10-30 17:27:59 +0000

    0. Simple rectangle fix indentation

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 """
-    This modle definse a class Rectangle
+This modle definse a class Rectangle
 """
 
 
 class Rectangle:
     """ 
-        Empty class that deifnes a Rectangle
+    Empty class that deifnes a Rectangle
     """
 
     pass
