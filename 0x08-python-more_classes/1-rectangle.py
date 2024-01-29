0827a997846f9bef4009bde3e5807fb3a654bb81
Obada-Badee <obadabadee3@hotmail.com>
2024-01-29 13:23:04 +0000

    new code

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,49 +1,48 @@
 #!/usr/bin/python3
 
-"""
-This modle definse a class Rectangle
-"""
+"""This module contains a Class named Rectangle
+with a specific width an hieght"""
 
 
 class Rectangle:
-    """
-    A class that deifnes a Rectangle
-    """
+    """ A class that defines a Rectangle with specific width and height"""
 
     def __init__(self, width=0, height=0):
-        """ Initilze the width and height"""
+        """Initialize the Rectangle class"""
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """ Returns the width"""
+        """ Return the width attribute"""
 
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        """ Sets the vlaue of the width"""
+        """ Set a new value for the width"""
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if vlaue < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
+
         self.__width = value
 
     @property
     def height(self):
-        """ Returns the height"""
+        """ Return the height attribute"""
 
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        """ Sets the vlaue of the height"""
+        """ Set a new value for the height"""
 
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if vlaue < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
+
         self.__height = value
