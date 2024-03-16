9eeebdabd867d1332872de4b5b40d49e445cb928
Obada-Badee <obadabadee3@hotmail.com>
2024-03-16 22:26:09 +0300

    1-filter_states.py add binary

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-script should take 3 arguments: mysql username, mysql password and database name
+a script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
+script should take 3 arguments:
+    mysql username, mysql password and database name
 """
 
 import MySQLdb
@@ -15,8 +17,8 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
-    
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
+
     for row in cursor.fetchall():
         print(row)
 
