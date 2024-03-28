906d552870d7e24a769ddb9344ce80a700e2fa8b
Obada-Badee <obadabadee3@hotmail.com>
2024-03-29 02:40:59 +0300

    remove try except

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,13 +13,13 @@ if __name__ == '__main__':
     q = argv[1] if len(argv) == 2 else ""
     url = 'http://0.0.0.0:5000/search_user'
     r = requests.post(url, data={'q': q})
+    r_dict = r.json()
 
-    try:
-        r_dict = r.json()
+    if (type(json) is dict):
         id, name = r_dict.get('id'), r_dict.get('name')
         if len(r_dict) == 0 or not id or not name:
             print("No result")
         else:
             print(f"[{r_dict.get('id')}] {r_dict.get('name')}")
-    except:
+    else:
         print("Not a valid JSON")
