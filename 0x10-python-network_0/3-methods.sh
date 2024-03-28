5183ca6bcd0c2b82f290b05281dc9a86b29565b1
Obada-Badee <obadabadee3@hotmail.com>
2024-03-28 04:36:29 +0300

    Make file exe

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods
-curl -sIX OPTIONS 0.0.0.0:5000/route_4 | grep Allow | cut -d ' ' -f2-
+curl -sIX OPTIONS "$1" | grep Allow | cut -d ' ' -f2-
