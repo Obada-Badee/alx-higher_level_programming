5b672540bc20b7449472b0cbd5e7b5b564c158fa
Obada-Badee <obadabadee3@hotmail.com>
2024-03-29 02:19:36 +0300

    $1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-A python Script Fetch a url and display the value of
+Fetch a url and display the value of
 the X-Request-Id variable found
 in the header of the response
 """
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     from sys import argv
 
     r = requests.get(argv[1])
-    xid = r.headers['X-Request-Id']
-    print(xid)
+    x_id = r.headers.get('X-Request-Id')
+    print(x_id)
