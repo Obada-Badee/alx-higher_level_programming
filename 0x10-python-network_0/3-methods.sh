4f133617eacf86ff58d4667858cc078fc75835e1
Obada-Badee <obadabadee3@hotmail.com>
2024-03-28 04:34:38 +0300

    Modify 3-methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods
-curl -sILX OPTIONS "$1" | grep Allow
+curl -sIX OPTIONS 0.0.0.0:5000/route_4 | grep Allow | cut -d ' ' -f2-
