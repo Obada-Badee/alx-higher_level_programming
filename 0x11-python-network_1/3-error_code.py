1b63b349b86c16abefec0d4a59a59256d32bab8d
Obada-Badee <obadabadee3@hotmail.com>
2024-03-29 01:44:38 +0300

    $1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-# Take in a URL, send a request to URL, and dispaly body
+"""
+Take in a URL, send a request to URL, and dispaly body
+"""
 
 
-import sys
+from sys import argv
 import urllib.request
 from urllib.error import HTTPError
 
 if __name__ == "__main__":
-    req = urllib.request.Request(sys.argv[1])
     try:
-        with urllib.request.urlopen(req) as res:
+        with urllib.request.urlopen(argv[1]) as res:
             print(res.read().decode('utf-8'))
     except HTTPError as e:
         print(f'Error code: {e.code}')
